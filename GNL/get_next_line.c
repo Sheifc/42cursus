1a2231365712cff8cbf07b3b1788ea55a036b5bb
SFC <115345487+Sheifc@users.noreply.github.com>
2024-01-10 22:56:14 +0100

    Add files via upload

---
+++ GNL/get_next_line.c
@@ -0,0 +1,100 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sheferna <sheferna@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/01 15:38:03 by sheferna          #+#    #+#             */
+/*   Updated: 2023/11/19 19:07:30 by sheferna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_free(char **str)
+{
+	free(*str);
+	*str = NULL;
+	return (NULL);
+}
+
+char	*ft_read(int fd, char *estatic)
+{
+	int		nbytes;
+	char	*buffer;
+
+	nbytes = 1;
+	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buffer)
+		return (ft_free(&estatic));
+	buffer[0] = '\0';
+	while (nbytes > 0 && !ft_strchr(buffer, '\n'))
+	{
+		nbytes = read (fd, buffer, BUFFER_SIZE);
+		if (nbytes > 0)
+		{
+			buffer[nbytes] = '\0';
+			estatic = ft_strjoin_gnl(estatic, buffer);
+		}
+	}
+	free(buffer);
+	if (nbytes == -1)
+		return (ft_free(&estatic));
+	return (estatic);
+}
+
+char	*ft_get_line(char *estatic)
+{
+	char	*line;
+	char	*ptr;
+	int		len;
+
+	ptr = ft_strchr(estatic, '\n');
+	len = (ptr - estatic) + 1;
+	line = ft_substr(estatic, 0, len);
+	if (!line)
+		return (NULL);
+	return (line);
+}
+
+char	*ft_new_estatic(char *estatic)
+{
+	char	*new_estatic;
+	char	*ptr;
+	int		len;
+
+	ptr = ft_strchr(estatic, '\n');
+	if (!ptr)
+	{
+		new_estatic = NULL;
+		return (ft_free(&estatic));
+	}
+	else
+		len = (ptr - estatic) + 1;
+	if (!estatic[len])
+		return (ft_free(&estatic));
+	new_estatic = ft_substr(estatic, len, ft_strlen(estatic) - len);
+	ft_free(&estatic);
+	if (!new_estatic)
+		return (NULL);
+	return (new_estatic);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*estatic = NULL;
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	if ((estatic && !ft_strchr(estatic, '\n')) || !estatic)
+		estatic = ft_read (fd, estatic);
+	if (!estatic)
+		return (NULL);
+	line = ft_get_line(estatic);
+	if (!line)
+		return (ft_free(&estatic));
+	estatic = ft_new_estatic(estatic);
+	return (line);
+}
---
+++ GNL/get_next_line_utils_bonus.c
@@ -0,0 +1,107 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sheferna <sheferna@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/01 15:38:03 by sheferna          #+#    #+#             */
+/*   Updated: 2024/01/10 22:53:26 by sheferna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+size_t	ft_strlen(char *s)
+{
+	size_t	len;
+
+	len = 0;
+	while (*s++)
+		len++;
+	return (len);
+}
+
+char	*ft_strchr(char *s, int c)
+{
+	while (*s != (char)c)
+	{
+		if (!*s)
+			return (NULL);
+		s++;
+	}
+	return ((char *)s);
+}
+
+char	*ft_strdup(char *s1)
+{
+	size_t	len;
+	size_t	i;
+	char	*p;
+
+	len = ft_strlen(s1) + 1;
+	p = (char *)malloc(sizeof(char) * len);
+	i = -1;
+	if (!p)
+		return (0);
+	while (s1[++i])
+		p[i] = s1[i];
+	p[i] = '\0';
+	return (p);
+}
+
+char	*ft_strjoin_gnl(char *s1, char *s2)
+{
+	char	*str;
+	size_t	i;
+	size_t	j;
+
+	if (!s1)
+	{
+		s1 = ft_strdup("");
+		if (!s1)
+			return (0);
+	}
+	str = malloc(sizeof(char) * ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!str)
+		return (ft_free(&s1));
+	i = 0;
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j])
+		str[i++] = s2[j++];
+	str[i] = '\0';
+	free(s1);
+	return (str);
+}
+
+char	*ft_substr(char *s, unsigned int start, size_t len)
+{
+	size_t	i;
+	char	*str;
+
+	i = 0;
+	if (!s)
+		return (0);
+	if (start > ft_strlen(s))
+	{
+		str = malloc(sizeof(char) * (1));
+		if (!str)
+			return (NULL);
+		str[0] = '\0';
+		return (str);
+	}
+	if (ft_strlen(s) - start < len)
+		len = ft_strlen(s) - start;
+	str = malloc(sizeof(char) * (len + 1));
+	if (!str)
+		return (NULL);
+	while (start < ft_strlen(s) && i < len && s[start])
+		str[i++] = s[start++];
+	str[i] = '\0';
+	return (str);
+}
