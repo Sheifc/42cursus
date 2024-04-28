5e5867b2094ff4bcb6552e2bbf23434da7d36c1e
SFC <115345487+Sheifc@users.noreply.github.com>
2024-04-28 22:03:07 +0200

    Update rotone.c

--- Rank2_Exam/Level1/rotone.c
+++ Rank2_Exam/Level1/rotone.c
@@ -30,30 +30,6 @@ $> */
 
 #include <unistd.h>
 
-/* int main(int argc, char **argv)
-{
-	int i;
-
-	i = 0;
-	if (argc == 2)
-	{
-		while (argv[1][i])
-		{
-			if (argv[1][i] >= 'a' && argv[1][i] <= 'z')
-				argv[1][i] = (argv[1][i] + 1 - 'a') % 26 + 'a';
-			else if (argv[1][i] >= 'A' && argv[1][i] <= 'Z')
-				argv[1][i] = (argv[1][i] + 1 - 'A') % 26 + 'A';
-			write(1, &argv[1][i], 1);
-			i++;
-		}
-	}
-	write(1, "\n", 1);
-} */
-
-// Otro modo: 
-
-#include <unistd.h>
-
 int main (int argc, char **argv)
 {
     if(argc == 2)
@@ -80,3 +56,25 @@ int main (int argc, char **argv)
 	}
     write(1,"\n",1);
 }
+
+/* Otro modo: 
+
+int main(int argc, char **argv)
+{
+	int i;
+
+	i = 0;
+	if (argc == 2)
+	{
+		while (argv[1][i])
+		{
+			if (argv[1][i] >= 'a' && argv[1][i] <= 'z')
+				argv[1][i] = (argv[1][i] + 1 - 'a') % 26 + 'a';
+			else if (argv[1][i] >= 'A' && argv[1][i] <= 'Z')
+				argv[1][i] = (argv[1][i] + 1 - 'A') % 26 + 'A';
+			write(1, &argv[1][i], 1);
+			i++;
+		}
+	}
+	write(1, "\n", 1);
+} */
